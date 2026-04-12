8155c29366600f2713d9f7dce26f5b9ffc8d326e
Chandima Karunaratne <c.karunaratne.mudiyanselage.10@student.scu.edu.au>
2026-04-12 15:54:11 +1000

    launch screen fixes

--- bookmarkapp/bookmarkappApp.swift
+++ bookmarkapp/bookmarkappApp.swift
@@ -13,33 +13,17 @@ import UIKit
 
 @main
 struct bookmarkappApp: App {
-    @State private var showSplash = true
-    
     init() {
         configureTabBarAppearance()
     }
-    
+
     var body: some Scene {
         WindowGroup {
-            ZStack {
-                RootTabView()
-                    .opacity(showSplash ? 0 : 1)
-
-                if showSplash {
-                    SplashScreenView()
-                        .transition(.opacity)
-                }
-            }
-            .animation(.easeInOut(duration: 0.4), value: showSplash)
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
-                    showSplash = false
-                }
-            }
+            RootTabView()
         }
         .modelContainer(for: [Book.self, Quote.self])
     }
-    
+
     private func configureTabBarAppearance() {
         #if canImport(UIKit)
         UITabBar.appearance().unselectedItemTintColor = UIColor(
